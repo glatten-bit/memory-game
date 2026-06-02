bfe4eab6acb5e30348061aa4cb6b88417c167181
glatten-bit <glatten@mecklenburg.org>
2026-06-01 21:33:48 -0400

    Updated Memory Image Mapping

--- memoryPairs.js
+++ memoryPairs.js
@@ -1,56 +1,56 @@
 const memoryCards = [
   {
     id: 1,
-    image: "/images/card1.png",
+    image: "images/card1.png",
     matchId: "card1"
   },
   {
     id: 2,
-    image: "/images/card1.png",
+    image: "images/card1.png",
     matchId: "card1"
   },
 
   {
     id: 3,
-    image: "/images/card2.png",
+    image: "images/card2.png",
     matchId: "card2"
   },
   {
     id: 4,
-    image: "/images/card2.png",
+    image: "images/card2.png",
     matchId: "card2"
   },
 
   {
     id: 5,
-    image: "/images/card3.png",
+    image: "images/card3.png",
     matchId: "card3"
   },
   {
     id: 6,
-    image: "/images/card3.png",
+    image: "images/card3.png",
     matchId: "card3"
   },
 
   {
     id: 7,
-    image: "/images/card4.png",
+    image: "images/card4.png",
     matchId: "card4"
   },
   {
     id: 8,
-    image: "/images/card4.png",
+    image: "images/card4.png",
     matchId: "card4"
   },
 
   {
     id: 9,
-    image: "/images/card5.png",
+    image: "images/card5.png",
     matchId: "card5"
   },
   {
     id: 10,
-    image: "/images/card5.png",
+    image: "images/card5.png",
     matchId: "card5"
   }
 ];
